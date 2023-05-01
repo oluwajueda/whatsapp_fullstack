65b272cdc114dea3d0469eb532ce96d7dfbf564f
oluwajueda <oluwajuedaloade@gmail.com>
2023-05-01 18:54:37 +0100

    create web view

--- lib/screens/web_layout_screen.dart
+++ lib/screens/web_layout_screen.dart
@@ -1,11 +1,31 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:whatsapp_full_stack/widgets/contacts_lists.dart';
+import 'package:whatsapp_full_stack/widgets/web_profile_bar.dart';
+import 'package:whatsapp_full_stack/widgets/web_search_bar.dart';
 
 class WebLayoutScreen extends StatelessWidget {
   const WebLayoutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+              child: SingleChildScrollView(
+            child: Column(
+              children: const [
+                WebProfileBar(),
+                WebSearchBar(),
+                ContactsList(),
+              ],
+            ),
+          ))
+        ],
+      ),
+    );
   }
 }
