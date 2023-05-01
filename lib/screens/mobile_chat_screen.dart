c78fc3afd2afcd55157a5186bcf4129169ebe858
oluwajueda <oluwajuedaloade@gmail.com>
2023-05-01 10:05:12 +0100

    create message card

--- lib/screens/mobile_chat_screen.dart
+++ lib/screens/mobile_chat_screen.dart
@@ -1,11 +1,25 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:whatsapp_full_stack/colors.dart';
+import 'package:whatsapp_full_stack/info.dart';
 
 class MobileChatScreen extends StatelessWidget {
   const MobileChatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        title: Text(info[0]["name"].toString()),
+        centerTitle: false,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.video_call)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/chat_list.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:whatsapp_full_stack/colors.dart';
+import 'package:whatsapp_full_stack/info.dart';
+import 'package:whatsapp_full_stack/widgets/my_message_card.dart';
+
+class ChatList extends StatelessWidget {
+  const ChatList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: messages.length,
+      itemBuilder: (context, index) {
+        if (messages[index]["isMe"] == true) {
+          return MyMessageCard(
+            message: messages[index]["text"].toString(),
+            date: messages[index]["time"].toString(),
+          );
+        }
+        // return senderMessageCard(
+
+        // )
+      },
+    );
+  }
+}
