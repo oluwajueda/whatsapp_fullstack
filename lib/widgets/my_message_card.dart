b3316b6b56b3156f228c8aa76965d85598b4d175
oluwajueda <oluwajuedaloade@gmail.com>
2023-05-05 23:07:07 +0100

    remove unecessary imports

--- lib/widgets/my_message_card.dart
+++ lib/widgets/my_message_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+
 import 'package:whatsapp_full_stack/colors.dart';
 
 class MyMessageCard extends StatelessWidget {
